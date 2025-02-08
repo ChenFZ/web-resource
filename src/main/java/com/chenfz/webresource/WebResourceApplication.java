9319d9b7ca624752d044d9591bfe40b86615a3c6
chenfz <13277424603@163.com>
2025-02-08 18:04:24 +0800

    initial commit

--- src/main/java/com/chenfz/webresource/WebResourceApplication.java
+++ src/main/java/com/chenfz/webresource/WebResourceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebResourceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebResourceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebResourceApplication.class, args);
+    }
 
 }
