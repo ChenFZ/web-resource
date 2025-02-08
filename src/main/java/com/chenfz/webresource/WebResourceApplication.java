b3549583a9414c4701a76d0e378aa40f126c9774
chenfz <13277424603@163.com>
2025-02-08 18:01:41 +0800

    first commit

---
+++ src/main/java/com/chenfz/webresource/WebResourceApplication.java
@@ -0,0 +1,13 @@
+package com.chenfz.webresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chenfz/webresource/WebResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenfz.webresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
